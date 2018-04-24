240a7a5663b6bf9ce80d06f94a630e9977594d08
Sturtevant <asturtevant@mm206211-pc.mitre.org>
2018-04-24 12:55:05 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# JRE8-hardening
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: JRE8-hardening
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
