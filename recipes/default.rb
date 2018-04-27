dc551f1334f339787933f51db5159be73375088e
Sturtevant <asturtevant@mm206211-pc.mitre.org>
2018-04-27 08:24:00 -0400

    Initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,46 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+include_recipe 'java'
+
+directory '/etc/.java/' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/etc/.java/deployment' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+template '/etc/.java/deployment/deployment.config' do
+  source 'deployconfig.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+template '/etc/.java/deployment/deployment.properties' do
+  source 'deployproperties.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+file '/etc/.java/deployment/exception.sites' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+
+
+
